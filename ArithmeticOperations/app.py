63c4a54c920b349ae7c0d3d5ec966b2164ab2704
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2020-01-16 12:38:34 +0600

    Arithmetic operations and operator precedence

---
+++ ArithmeticOperations/app.py
@@ -0,0 +1,15 @@
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+
+print('-' * 15)
+
+# operator precedence
+x = 10 + 3 * 2
+print(x)
+
+x = (10 + 3) * 2
+print(x)
