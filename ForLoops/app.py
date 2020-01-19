1ac69827ae85ec650acf24fd8a65e42f3b2f43a7
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2020-01-19 20:52:16 +0600

    Example of using for loop

---
+++ ForLoops/app.py
@@ -0,0 +1,12 @@
+# for item in range(10):
+#     print(item)
+
+# Task: calculate the sum of items
+# prices = [10, 20, 30]
+
+# Solution:
+prices = [10, 20, 30]
+total = 0
+for i in prices:
+    total += i
+print(total)
