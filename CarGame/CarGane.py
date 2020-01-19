27c81bf43bd727c356b9594ae4d0fc5e340d0a41
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2020-01-19 20:32:36 +0600

    Modified CarGame - can not start and stop the car if it is already started or stopped

--- CarGame/CarGane.py
+++ CarGame/CarGane.py
@@ -1,11 +1,19 @@
 command = ''
-
+car_started = False
 while True:
     command = input('> ').lower()
     if command == 'start':
-        print('Car started...Ready to go! ')
+        if car_started:
+            print('Car is already started...')
+        else:
+            car_started = True
+            print('Car started...Ready to go! ')
     elif command == 'stop':
-        print('Car stopped.')
+        if not car_started:
+            print('Car is already stopped...')
+        else:
+            car_started = False
+            print('Car stopped.')
     elif command == 'help':
         print('''
 start - to start the car
