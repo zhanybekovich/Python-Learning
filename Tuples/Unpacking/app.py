1edb12642ae1b8a5f6598521289a2322246e2a8a
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2020-01-22 20:13:42 +0600

    Example of unpacking tuples

---
+++ Tuples/Unpacking/app.py
@@ -0,0 +1,11 @@
+coordinates = (1, 2, 3)
+# unpacking
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+
+# pythonic way of unpacking
+x, y, z = coordinates
+print(x)
+print(z)
+print(y)
