64c1af4175a56d94a509bae315b4fb608a33879d
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2020-01-23 20:00:53 +0600

    Example of keyword arguments

---
+++ Functions/keyword_arguments.py
@@ -0,0 +1,8 @@
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome aboard')
+
+
+print('Start')
+greet_user(last_name='John', first_name='Smith')
+print('Finish')
