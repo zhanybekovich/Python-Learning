aa4f3ea44adff13a04309309db3074051e5ba82a
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2020-01-24 19:42:01 +0600

    Example of inheritance

---
+++ Classes/Inheritance/app.py
@@ -0,0 +1,22 @@
+class Mammal:
+    def walk(self):
+        print('walk')
+
+
+class Dog(Mammal):
+    def bark(self):
+        print('bark')
+
+
+class Cat(Mammal):
+    def be_annoying(self):
+        print('annoying')
+
+
+dog1 = Dog()
+dog1.walk()
+dog1.bark()
+
+
+cat1 = Cat()
+cat1.be_annoying()
