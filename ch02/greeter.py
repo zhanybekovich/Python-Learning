4fbc11b399bb0daf67c2234f71054172dbea3855
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2020-02-04 08:25:35 +0600

    example of vars

---
+++ ch02/greeter.py
@@ -0,0 +1,4 @@
+name = "Вася"
+print(name)
+print("Привет,", name)
+input("\nНажмите Enter, чтобы выйти. ")
---
+++ ch02/personal_greeter.py
@@ -0,0 +1,4 @@
+name = input("Привет, Как тебя зовут? ")
+print(name)
+print("Привет,", name)
+input("\n\nНажмите Enter, чтобы выйти. ")
