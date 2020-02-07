ee66d540647cfe977182d77fd85c359a43ac42b9
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2020-02-07 16:22:07 +0600

    example of for loop

---
+++ ch04/counter.py
@@ -0,0 +1,12 @@
+print("Посчитаем:")
+for i in range(10):
+    print(i, end=" ")
+
+print("\nПосчитаем в обратном порядке:")
+for i in range(0, 50, 5):
+    print(i, end=" ")
+
+print("\n\nCounting backwards:")
+for i in range(10, 0, -1):
+    print(i, end=" ")
+
---
+++ ch04/loopy_string.py
@@ -0,0 +1,4 @@
+word = input("Введите слово: ")
+print("\nВот все буквы вашего слова по порядку:")
+for letter in word:
+    print(letter)
