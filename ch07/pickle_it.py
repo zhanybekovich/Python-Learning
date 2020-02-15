5b17f3dfee43731f5bc657227ef1653bbb4336a1
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2020-02-15 20:42:43 +0600

    pickling data

--- ch07/pickle_it.py
+++ ch07/pickle_it.py
@@ -21,3 +21,16 @@ print(variety)
 print(shape)
 print(brand)
 f.close()
+
+print("\nПомещение списков на полку.")
+s = shelve.open("pickles2.dat")
+s["variety"] = ["огурцы", "помидоры", "капуста"]
+s["shape"] = ["целые", "кубиками", "соломкой"]
+s["brand"] = ["Главпродукт", "Чумак", "Бондюэль"]
+s.sync()
+
+print("\nИзвлечение списков из файла полки:")
+print("торговые марки - ", s["brand"])
+print("формы - ", s["shape"])
+print("виды овощей - ", s["variety"])
+s.close()
